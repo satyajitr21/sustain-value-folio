95b8a4a752595e1811d33caa19eeebc27c5e0d35
Sourav Dey <dey.sourav777@gmail.com>
2025-08-09 20:56:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: 'sustain-value-folio',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
