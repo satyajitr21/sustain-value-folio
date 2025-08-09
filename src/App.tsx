d7b64f2c44cb3548e9a43b63143a6a1e91f929aa
Sourav Dey <dey.sourav777@gmail.com>
2025-08-09 21:25:39 +0530

    router fixed

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <div className="min-h-screen flex flex-col">
           <Navigation />
           <main className="flex-1">
