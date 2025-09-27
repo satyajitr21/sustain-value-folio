25d0bcc521d4b2de51e57549afc5f2f9fda27774
satyajitr21 <satyajitroy41@gmail.com>
2025-09-27 23:03:29 +0530

    Update Blog71.tsx

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -43,7 +43,7 @@ This raises intriguing unknowns: Will generational differences amplify the spill
 
 These are just thoughts; I do not have answers to or well-thought-out opinions on any of these.
 
-***Disclaimer:*** The thoughts are personal. My knowledge of the AI ecosystem is limited; I'm drawing from articles, studies, and observations, not exhaustive expertise. I'd love to hear your thoughts, counterarguments, or additional insights in the comments. Also, I sought Grok's help to draft parts of the article.
+<strong><em>Disclaimer:<strong><em> The thoughts are personal. My knowledge of the AI ecosystem is limited; I'm drawing from articles, studies, and observations, not exhaustive expertise. I'd love to hear your thoughts, counterarguments, or additional insights in the comments. Also, I sought Grok's help to draft parts of the article.
 `,
     date: "2025-09-27",
     readTime: "7 min read",
